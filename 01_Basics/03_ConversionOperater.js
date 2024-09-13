0c035f8c60e381e476d9c9f8dde69386eb042505
smiittt <smitmiyani5615@gmail.com>
2024-09-14 00:17:20 +0530

    first commit

---
+++ 01_Basics/03_ConversionOperater.js
@@ -0,0 +1,20 @@
+let name = "13Smit"
+console.log(name);
+console.log(typeof  (name));
+
+let nameToNumber = Number(name)
+console.log(typeof  nameToNumber);
+console.log(nameToNumber);
+
+
+// "Smit" => Smit
+// "Smitabc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Smit"
+console.log(Boolean (isLoggedIn));
+
+// 1 => true; 0 => false
+// "" => false
+// "Smit" => true
+
