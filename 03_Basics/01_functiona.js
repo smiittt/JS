a96a005effd00e4c1b4409d0f5e6a1099a0a938a
smiittt <smitmiyani5615@gmail.com>
2024-09-18 18:59:47 +0530

    01 Function - taking perameter

---
+++ 03_Basics/01_functiona.js
@@ -0,0 +1,34 @@
+function MyName() {
+  console.log("Smit");
+  console.log("Miyani");
+}
+
+MyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){  // "sam" is default value if user isnot entered
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
