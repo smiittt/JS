e8fd26f4f9f9b171041cc752456183cf0a51ebd9
smiittt <smitmiyani5615@gmail.com>
2024-10-03 19:52:32 +0530

    03 DOM - Nodelist/Selectelement/CreateElement

---
+++ pr.js
@@ -0,0 +1,21 @@
+const name = "Smit Miyani";
+for (const element of name) {
+  // console.log(`Your name each char : ${element}`);
+}
+
+// maps
+
+const okay = new Map();
+
+okay.set("Smit", "92xx28xxx4");
+okay.set("City", "Surat");
+okay.set("Country", "India");
+okay.set("Smit", "92xx28xxx4");
+
+for (const [key, values] of okay) {
+  // console.log(key +":-" + values);
+}
+
+const obj = {
+    
+};
