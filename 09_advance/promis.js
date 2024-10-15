882dc9a1fdadf39183c122b702c6be7b1052d858
smiittt <smitmiyani5615@gmail.com>
2024-10-15 20:30:20 +0530

    09 Async Await (Promises part-2) Completed

--- 09_advance/promis.js
+++ 09_advance/promis.js
@@ -53,4 +53,52 @@ promisFour
   })
   .catch(function (error) {
     console.log(error);
+  })
+  .finally(() => {
+    console.log("All Done!!");
   });
+
+const promisFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let errorr = true;
+    if (errorr) {
+      reject("Error : Something went wrong");
+    } else {
+      resolve({ myname: "Smit Miyani", company: "Bugrifix Software Pvt Ltd" });
+    }
+  }, 1000);
+});
+
+async function promisFiveUsingAsyncAwait() {
+  try {
+    const response = await promisFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+promisFiveUsingAsyncAwait()
+
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/smiittt')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
