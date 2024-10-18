86b3bbe0000e03fe9c87e8fd39a56b6342464aa5
smiittt <smitmiyani5615@gmail.com>
2024-10-19 01:27:18 +0530

    10 prototype part-2 (Creating own prototype and injecting in object so that can be useable for all object)

--- 10_oop_and_class/prototype.js
+++ 10_oop_and_class/prototype.js
@@ -1,27 +1,69 @@
 let cars = ["BMW", "Audi", "Farari "];
 
 let carDetails = {
-    bmw : "Normal car",
-    Audi : "150+ Speed",
+  bmw: "Normal car",
+  Audi: "150+ Speed",
 
-    AudiSpeed:function(){
-        console.log(`Audi cars normal speed is : ${this.audi}`);
-        
-    }
+  AudiSpeed: function () {
+    console.log(`Audi cars normal speed is : ${this.audi}`);
+  },
+};
+
+Object.prototype.color = function () {
+  console.log(`color is present in all objects`);
+};
+
+Array.prototype.allpetrol = function () {
+  console.log(`Yes, All cars are in petrol`);
+};
+
+// cars.color();
+// cars.allpetrol();
+
+// carDetails.color();
+// carDetails.allpetrol();
+
+
+
+
+// Inheritance
+
+const user = {
+  f_name: "Smit",
+  l_name: "Miyani",
+};
+const LoginDetails = {
+  isLoggedIn: false,
+  todayLoggedIn: false,
+  __proto__:user, // All prooerties can accessible from loginDetails
+  login: function () {
+    console.log(`Sorry ${this.f_name}, Try again tommorow`)
+  }
+};
+const Authority = {
+    isAuthority: true,
+    isSuperAuthority: false,
+
+    //__proto__: LoginDetails // All loginDetails properties are accessible through Authority!
+};
+
+
+
+
+// modern syntax of inheritance
+Object.setPrototypeOf(Authority, LoginDetails)
+
+console.log(Authority.login());
+
+
+
+// making  prototype with customName
+
+let CompanyName = "Bugrifix Software Pvt Ltd"
+
+Object.prototype.trueLength = function () {
+    console.log(`${this}`)
+    console.log(` True length is : ${this.trim().length}`)
 }
 
-
-Object.prototype.color = function(){
-    console.log(`color is present in all objects`);
-}
-
-Array.prototype.allpetrol = function(){
-    console.log(`Yes, All cars are in petrol`);
-}
-
-
-cars.color()
-cars.allpetrol()
-
-carDetails.color()
-carDetails.allpetrol()
+CompanyName.trueLength()
